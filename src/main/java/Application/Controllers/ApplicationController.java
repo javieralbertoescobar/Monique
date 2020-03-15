04f735afe2998928103e6f1daf5977a89a3722ee
Javier20105 <xavierescobar3335@gmail.com>
2020-03-15 14:07:12 -0300

    Se implemento el alta de producto

--- src/main/java/Application/Controllers/ApplicationController.java
+++ src/main/java/Application/Controllers/ApplicationController.java
@@ -4,30 +4,30 @@ import Application.Model.Product;
 import Application.Repositories.ProductRepository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
 
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping(path = "/demo")
+@RequestMapping(path = "/api")
 public class ApplicationController {
     @Autowired
     private ProductRepository productRepository;
 
-    @PostMapping(path = "/add") // Map ONLY POST Requests
-    public @ResponseBody
-    String addNewUser(@RequestParam Integer id, @RequestParam String name
-            , @RequestParam Double price) {
-        Product p = new Product();
-        p.setId(id);
-        p.setName(name);
-        p.setPrice(price);
+    @PostMapping(path = "/products") // Map ONLY POST Requests
+    public String createProduct(@Valid @RequestBody Product p) {
+        //p.setId(0);//este campo es auto incremental
+        //p.setName(name);
+        //p.setPrice(price);
         productRepository.save(p);
         return "Producto guardado exitosamente";
 
     }
 
-    @GetMapping(path = "/all")
-    public @ResponseBody
-    Iterable<Product> getAllUsers() {
+
+   // @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(path = "/products")
+    public Iterable<Product> getAllProducts() {
         // This returns a JSON or XML with the users
         return productRepository.findAll();
     }
